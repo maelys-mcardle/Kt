94c2c7f3f0e1d944cceac4e47c5a2dfdd2efe4e1
julien <julien@jmcardle.com>
2012-01-09 12:12:33 -0500

    Start of project. Korgi icon.

---
+++ icon/export.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+# Function to export SVG at different resolutions.
+generate_png () {
+	inkscape --export-png=korgi_$1px.png \
+	--export-width=$1 --export-height=$1 korgi.svg
+}
+
+# Call the function.
+generate_png 64
+generate_png 128
+generate_png 512
