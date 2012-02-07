41403e81c21ad99a296986170e9c33cf70b126a9
julien <julien@jmcardle.com>
2012-02-07 14:02:40 -0500

    Primitives work.

--- code/frontend/main.js
+++ code/frontend/main.js
@@ -1,10 +1,36 @@
- 
-function main() {
-	var canvas = document.getElementById( 'surface' );
-	var context = canvas.getContext( '2d' );
+function style() 
+{
+	this.red = "red";
+}
 
-	canvas.width  = window.innerWidth;
-	canvas.height = window.innerHeight;
-	context.fillStyle = "red";
-	context.fillRect(0,0,canvas.width,canvas.height);
+function surface()
+{
+	this.canvas = document.getElementById("surface");
+	this.context = this.canvas.getContext("2d");
+	
+	surface.prototype.update = function()
+	{
+		this.width = this.canvas.width = window.innerWidth;
+		this.height = this.canvas.height = window.innerHeight;
+	}
+	
+	surface.prototype.background = function(style)
+	{
+		this.rectangle(0, 0, this.canvas.width, this.canvas.height, style);
+	}
+	
+	surface.prototype.rectangle = function(x, y, width, height, style)
+	{
+		this.context.fillStyle = style;
+		this.context.fillRect(x, y, width, height);
+	}
+	
+}
+
+
+function main() 
+{
+	canvas = new surface();
+	canvas.update();
+	canvas.background("red");
 }
