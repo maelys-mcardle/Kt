e5f4b29bba9b10b4b6bd11061140063fc00d2c66
julien <julien@jmcardle.com>
2012-02-07 14:26:05 -0500

    Primitives work.

--- code/frontend/main.js
+++ code/frontend/main.js
@@ -1,36 +1,41 @@
-function style() 
-{
-	this.red = "red";
-}
+
+var style = {
+	"background" : "grey"
+};
 
 function surface()
 {
+	// Load in the canvas from the HTML5 DOM.
 	this.canvas = document.getElementById("surface");
 	this.context = this.canvas.getContext("2d");
 	
-	surface.prototype.update = function()
+	// Update canvas size to match the available surface area.
+	// Update local properties.
+	surface.prototype.update_geometry = function()
 	{
-		this.width = this.canvas.width = window.innerWidth;
+		this.width  = this.canvas.width  = window.innerWidth;
 		this.height = this.canvas.height = window.innerHeight;
 	}
 	
-	surface.prototype.background = function(style)
+	// Draw a background.
+	surface.prototype.draw_background = function(style)
 	{
-		this.rectangle(0, 0, this.canvas.width, this.canvas.height, style);
+		this.draw_rectangle(0, 0, this.canvas.width, this.canvas.height, style, "");
 	}
 	
-	surface.prototype.rectangle = function(x, y, width, height, style)
+	// Draw a rectangle.
+	surface.prototype.draw_rectangle = function(x, y, width, height, fill_style, stroke_style)
 	{
-		this.context.fillStyle = style;
+		this.context.strokeStyle = stroke_style;
+		this.context.fillStyle = fill_style;
 		this.context.fillRect(x, y, width, height);
 	}
 	
 }
 
-
 function main() 
 {
 	canvas = new surface();
-	canvas.update();
-	canvas.background("red");
+	canvas.update_geometry();
+	canvas.draw_background(style["background"]);
 }
