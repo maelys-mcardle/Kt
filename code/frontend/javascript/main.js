a499fa7e894538177eb04783e4dab5502c6af658
julien <julien@jmcardle.com>
2012-02-16 16:21:40 -0500

    Event loop.

--- code/frontend/javascript/main.js
+++ code/frontend/javascript/main.js
@@ -1,11 +1,10 @@
 
-function main() 
+function eventLoop(canvas)
 {
-	canvas = new kCanvas("surface");
 	canvas.setGeometry(window.innerWidth, window.innerHeight);
 	canvas.clear();
 	canvas.drawBackground(kStyle.background);
-	/*canvas.drawRectangle(10, 10, 30, 30, kStyle.rectangle);
+	canvas.drawRectangle(10, 10, 30, 30, kStyle.rectangle);
 	canvas.drawRoundedRectangle(50, 50, 30, 30, 15, kStyle.rectangle);
 	canvas.drawLine(100, 100, 50, 50, kStyle.rectangle);
 	canvas.drawLines(200, 100, [[10,10],[15,5],[4,3]], kStyle.rectangle);
@@ -15,9 +14,16 @@ function main()
 	canvas.drawBoundedText(400, 200, 500, 200, "So whis is not cool and that cat kool.", kStyle.text);
 	canvas.drawEllipse(600,100,200,200,kStyle.rectangle);
 	canvas.drawImage(20, 20, "../../icon/korgi_128px.png", kStyle.image);
-	canvas.drawBoundedImage(100, 100, 400, 400, "../../icon/korgi_128px.png", kStyle.image);*/
+	canvas.drawBoundedImage(100, 100, 400, 400, "../../icon/korgi_128px.png", kStyle.image);
 	canvas.setStyleGradient(400, 200, 500, 200, "white", "blue", kStyle.rectangle, "fillColour");
 	canvas.drawRectangle(400,200,500,200,kStyle.rectangle);
 	canvas.render();
-	setTimeout(main, 100);
+	
+	setTimeout(function(){eventLoop(canvas)}, 100);
+}
+
+function main(canvasId) 
+{
+	canvas = new kCanvas(canvasId);
+	eventLoop(canvas);
 }
