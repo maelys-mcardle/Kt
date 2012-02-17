6436b8c1248d765d467b3676c89f0fdee88304da
julien <julien@jmcardle.com>
2012-02-17 14:36:57 -0500

    Widget rendering essentials.

--- code/frontend/javascript/kWindow.js
+++ code/frontend/javascript/kWindow.js
@@ -18,10 +18,8 @@ function kWindow(kCanvasObject)
 kWindow.prototype.run = 
 function()
 {
-	this.canvas.setGeometry(window.innerWidth, window.innerHeight);
-	this.canvas.drawBackground(kStyle.background);
-	this.canvas.render();
-	
+	// Draw the background and the widgets.
+	this.drawWindow();
 	
 	// Wait before running the event loop again.
 	kWindowInstance = this;
@@ -36,4 +34,23 @@ kWindow.prototype.addWidget =
 function(kWidgetObject)
 {
 	this.widgets.push(kWidgetObject);
+}
+
+// =====================================================================
+// DRAW WINDOW
+// =====================================================================
+
+kWindow.prototype.drawWindow = 
+function()
+{
+	// Update the window properties, and set the background.
+	this.canvas.setGeometry(window.innerWidth, window.innerHeight);
+	this.canvas.drawBackground(kStyle.background);
+	
+	// Go widget by widget and draw it.
+	for (var i = 0; i < this.widgets.length; i++)
+		this.widgets[i].draw(this.canvas);
+	
+	// Render the canvas.
+	this.canvas.render();
 }
--- code/frontend/javascript/main.js
+++ code/frontend/javascript/main.js
@@ -3,5 +3,9 @@ function main(canvasId)
 {
 	canvas = new kCanvas(canvasId);
 	mainWindow = new kWindow(canvas);
+
+	button = new kButton(20, 20, 200, 200, "Hello");
+	mainWindow.addWidget(button);
+	
 	mainWindow.run();
 }
