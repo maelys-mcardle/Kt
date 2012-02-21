b9f698d3b9c215a2ea02e5dc7cb97b545e182a29
julien <julien@jmcardle.com>
2012-02-21 16:13:14 -0500

    Added out of bounds cursor handling.

--- code/frontend/javascript/kLineEdit.js
+++ code/frontend/javascript/kLineEdit.js
@@ -25,15 +25,29 @@ function kLineEdit(x, y, width, height, text)
 kLineEdit.prototype.draw = 
 function(canvas)
 {
+	// Calculate the width we have for the text.
+	var textWidth = this.width - 2 * this.style.padding;
+	
+	// If the cursor is *before* the displayed text, change the offset
+	// so the cursor remains in view.
+	if (this.cursorPosition < this.displayedTextOffset)
+		this.displayedTextOffset = this.cursorPosition;
+	
+	// If the cursor is *after* the displayable text, change the offset
+	// so that the cursor remains in view.
+	while (this.cursorPosition > this.displayedTextOffset + 
+		canvas.wrapTextToWidth(this.text.substring(
+		this.displayedTextOffset), textWidth, this.style).phrase.length)
+		this.displayedTextOffset++;
+	
 	// Draw the box containing the text.
 	canvas.drawRoundedRectangle(this.x, this.y, this.width, 
 		this.height, 5, this.style);
 	
 	// Draw the text.
 	this.displayedTextData = canvas.drawBoundedText(this.x + 
-		this.style.padding, this.y, this.width - 2 * this.style.padding,
-		this.height, this.text.slice(this.displayedTextOffset), 
-		this.style);
+		this.style.padding, this.y, textWidth, this.height, 
+		this.text.substring(this.displayedTextOffset), this.style);
 	
 	// Draw the cursor.
 	
@@ -83,13 +97,9 @@ function(mouseX, mouseY, character)
 	// User presses a key on the keyboard. Apply the action at the
 	// given cursor position.
 	
-	// Ignore newlines.
-	if (character == "\n") 
-		return;
-	
 	// Increase / decrease cursor position with arrow keys.
-	else if (character == kSpecialChar.rightArrow && 
-		this.cursorPosition + 1 < this.text.length)
+	if (character == kSpecialChar.rightArrow && 
+		this.cursorPosition < this.text.length)
 		this.cursorPosition++;
 	
 	else if (character == kSpecialChar.leftArrow && 
@@ -106,15 +116,15 @@ function(mouseX, mouseY, character)
 	
 	// Delete removes the character after the cursor.
 	else if (character == kSpecialChar.delete && 
-		this.cursorPosition < this.text.length - 1) {
+		this.cursorPosition < this.text.length) {
 		this.text = this.text.substring(0, this.cursorPosition) + 
 			this.text.substring(this.cursorPosition+1);
 	}
 	
-	// All other special characters are ignored.
-	if (character < 0) return;
+	// All special characters and newlines are ignored past this point.
+	if (character < 0 || character == "\n") return;
 		
-	// Otherwise append the character.
+	// Append the character to the text and update the cursor position.
 	this.text = this.text.substring(0, this.cursorPosition) + 
 		character + this.text.substring(this.cursorPosition);
 	this.cursorPosition++;
